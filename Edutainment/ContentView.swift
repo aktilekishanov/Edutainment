07872f154654f90ccd790062d5be6eba84155e2f
Aktilek Ishanov <aktilek.ishanov@gmail.com>
2023-01-16 21:26:43 +0600

    Initial Commit

---
+++ Edutainment/ContentView.swift
@@ -0,0 +1,161 @@
+//
+//  ContentView.swift
+//  Edutainment
+//
+//  Created by Aktilek Ishanov on 10.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isActive = false
+    @State private var rangeFrom = 2
+    @State private var rangeTo = 12
+    @State private var range = [Int]()
+    @State private var questionAmounts = [5, 10, 15]
+    @State private var questionAmount = 5
+    @State private var multiplicand = 0
+    @State private var multiplicator = 0
+    @State private var numbers = [Int]()
+    @State private var answers = [Int]()
+    @State private var results = [Int]()
+    @State private var score = 0
+    @State private var showingSubmitAnswers = true
+    @State private var showingScore = false
+    @FocusState private var answerIsFocused: Bool
+
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                if !isActive {
+                    List {
+                        Section {
+                            Stepper("From: \(rangeFrom)", value: $rangeFrom, in: 2...11, step: 1)
+                            Stepper("To: \(rangeTo)", value: $rangeTo, in: (rangeFrom + 1)...12, step: 1)
+                        } header: {
+                            Text("Multiplication range")
+                        }
+                        
+                        Section {
+                            Picker("Question amount", selection: $questionAmount) {
+                                ForEach(questionAmounts, id: \.self){
+                                    Text("\($0)")
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                        } header: {
+                            Text("Question amount")
+                        }
+
+                        
+                        HStack {
+                            Spacer()
+                            withAnimation {
+                                Button("Start Game", action: startGame)
+                                    .font(.headline)
+                            }
+                            Spacer()
+                        }
+                    }
+                }
+                else {
+                    List{
+                        Section {
+                            ForEach(0..<questionAmount) { i in
+                                HStack {
+                                    Text("\(numbers[2 * i])  x  \(numbers[2 * i + 1])  =")
+                                    TextField("Your answer", value: $answers[i], format: .number)
+                                        .keyboardType(.numberPad)
+                                        .focused($answerIsFocused)
+                                    
+                                    if showingScore {
+                                        Text(results[i] == 1 ? "Correct" : "Wrong")
+                                            .foregroundColor( results[i] == 1 ? .green : .red)
+                                    }
+                                }
+                            }
+                        } header: {
+                            Text("Calculate the following")
+                        }
+                        
+                        if showingScore {
+                            Section {
+                                Text("Score: \(score) / \(questionAmount)")
+                            } header: {
+                                Text("Results")
+                            }
+                        }
+                        
+                        if showingSubmitAnswers {
+                            HStack {
+                                Spacer()
+                                Button("Submit", action: submitAnswers)
+                                    .font(.headline)
+                                    .foregroundColor(.blue)
+
+                                Spacer()
+                            }
+                        }
+                        
+                        HStack {
+                            Spacer()
+                            Button("Restart", action: startGame)
+                                .font(.headline)
+                                .foregroundColor(.red)
+                            Spacer()
+                        }
+                    }
+                    .toolbar {
+                        ToolbarItemGroup(placement: .keyboard) {
+                            Spacer()
+                            Button("Done"){
+                                answerIsFocused = false
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Edutainment")
+        }
+    }
+    func startGame() {
+        numbers = [Int]()
+        results = [Int]()
+        answers = [Int]()
+        
+        isActive.toggle()
+        range = Array(rangeFrom...rangeTo)
+        for _ in 0..<(questionAmount * 2) {
+            numbers.append(Int.random(in: rangeFrom...rangeTo))
+            results.append(0)
+        }
+        
+        for _ in 0..<questionAmount {
+            answers.append(0)
+        }
+        score = 0
+        showingSubmitAnswers = true
+        showingScore = false
+
+
+    }
+    
+    func submitAnswers() {
+        for i in 0..<questionAmount {
+            if numbers[ 2 * i ] * numbers[ 2 * i + 1 ] == answers[i] {
+                score += 1
+                results[i] = 1
+            }
+        }
+        showingSubmitAnswers = false
+        showingScore = true
+    }
+
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Aktilek Ishanov on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
